e41c9c6aff88707415d89efbe673371422a8326c
dfuse06 <dfuse06@gmail.com>
2016-12-17 15:02:27 -0600

    first commit

---
+++ Kodi_installer.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+CL_BLU="\033[34m"
+CL_RST="\033[0m"
+
+echo -e ${CL_BLU}"                  _ _    _____           _        _ _              ${CL_BLU}";
+echo -e ${CL_BLU}"    /\ /\___   __| (_)   \_   \_ __  ___| |_ __ _| | | ___ _ __    ${CL_BLU}";
+echo -e ${CL_BLU}"   / //_/ _ \ / _\` | |    / /\/ '_ \/ __| __/ _\` | | |/ _ \ '__| ${CL_BLU}";
+echo -e ${CL_BLU}"  / __ \ (_) | (_| | | /\/ /_ | | | \__ \ || (_| | | |  __/ |      ${CL_BLU}";
+echo -e ${CL_BLU}"  \/  \/\___/ \__,_|_| \____/ |_| |_|___/\__\__,_|_|_|\___|_|      ${CL_BLU} ";
+echo -e ${CL_BLU}"                                                                   ${CL_RST}";
+
+echo -n "Enter device IP address.> "
+read text
+adb connect $text
+sleep 5
+echo "Now installing Kodi"
+cd ~/scripts/Kodi_Install/
+adb install org.xbmc.kodi_16.1.com.apk
+adb push script.areswizard0.0.52.zip /sdcard/
+adb disconnect $text
+echo "Done. Enjoy";
+
+
+
+
