c2c761cb5278ba726e6570763efbf500d15705f3
dfuse06 <dfuse06@gmail.com>
2017-10-29 21:59:29 -0500

    Fixed some shit

--- windows/Install_Krypton.bat
+++ windows/Install_Krypton.bat
@@ -5,6 +5,7 @@ set /p Input=Enter device IP address:
 adb connect "%Input%"
 echo Now Installing Kodi
 adb install kodi-17.4-Krypton-armeabi-v7a.apk
+adb push repository.aresproject-0.1.0.zip /sdcard/
 adb disconnect "%Input%"
 
 echo Press any key to exit...
